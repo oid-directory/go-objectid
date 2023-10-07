dcdd9a8b3e267c9c109accbac68b7d5728bd877f
JesseCoretta <jesse.coretta@icloud.com>
2023-10-07 02:18:52 -0700

    stable release prep

--- nf_test.go
+++ nf_test.go
@@ -1,14 +1,28 @@
 package objectid
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestNewNumberForm(t *testing.T) {
-	nf := `3849141823758536772162786183725055278`
-	if _, err := NewNumberForm(nf); err != nil {
-		t.Errorf("%s failed: %s", t.Name(), err.Error())
+	for idx, num := range []any{
+		`3849141823758536772162786183725055278`,
+		-103,
+		4874893,
+		``,
+	} {
+		_, err := NewNumberForm(num)
+		ok := err == nil
+		if !ok && idx%2 == 0 {
+			t.Errorf("%s failed: valid number not parsed: %v", t.Name(), err)
+			return
+		} else if ok && idx%2 != 0 {
+			t.Errorf("%s failed: bogus number parsed without error", t.Name())
+			return
+		}
 	}
+
 }
 
 func TestBogusNewNumberForm(t *testing.T) {
@@ -55,11 +69,44 @@ func TestNumberForm_Equal(t *testing.T) {
 		t.Errorf("%s failed: Gt evaluation returned a bogus value", t.Name())
 	}
 
-	if nf, _ = NewNumberForm(`437829765`); !nf.Equal(437829765) {
+	if nf, _ = NewNumberForm(`437829765`); !nf.Equal(`437829765`) {
 		t.Errorf("%s failed: Gt evaluation returned a bogus value", t.Name())
 	}
 
 	if nf, _ = NewNumberForm(uint64(329856)); !nf.Equal(uint64(329856)) {
 		t.Errorf("%s failed: Gt evaluation returned a bogus value", t.Name())
 	}
+}
+
+func ExampleNumberForm_Equal() {
+	nf1, _ := NewNumberForm(4658)
+	nf2, _ := NewNumberForm(4657)
+	fmt.Printf("Instances are equal: %t", nf1.Equal(nf2))
+	// Output: Instances are equal: false
+}
+
+func ExampleNumberForm_Valid() {
+	nf, _ := NewNumberForm(4658)
+	fmt.Printf("Valid: %t", nf.Valid())
+	// Output: Valid: true
+}
+
+func ExampleNumberForm_String() {
+	nf, _ := NewNumberForm(4658)
+	fmt.Printf("%s", nf)
+	// Output: 4658
+}
+
+func ExampleNumberForm_Gt() {
+	nf, _ := NewNumberForm(4658)
+	oth := 4501
+	fmt.Printf("%s > %d: %t", nf, oth, nf.Gt(oth))
+	// Output: 4658 > 4501: true
+}
+
+func ExampleNumberForm_Lt() {
+	nf, _ := NewNumberForm(4658)
+	oth := 4501
+	fmt.Printf("%s < %d: %t", nf, oth, nf.Lt(oth))
+	// Output: 4658 < 4501: false
 }
